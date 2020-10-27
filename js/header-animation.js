fe381149333fd2a5a53dc9444e684d0889b5897c
Adrian Kotliński <kotlinski95@gmail.com>
2020-10-27 08:48:14 +0100

    Upgrade tools html structure and introduction styles after index.html conflict

--- js/header-animation.js
+++ js/header-animation.js
@@ -1,31 +1,26 @@
 var previousScrollTop;
 var isScrolling;
-var nav = document.getElementsByClassName("main-header__wrapper");
-var scrollUp = document.getElementsByClassName("main-navigation__scroll-Up");
-
+var nav = document.getElementsByClassName('main-header__wrapper');
+var scrollUp = document.getElementsByClassName('main-navigation__scroll-Up');
 function hasScrolled() {
-  var scrollTop = window.scrollY;
-  if (scrollTop > previousScrollTop) {
-    nav[0].classList.add("main-header__wrapperUp");
-    scrollUp[0].style.display = "initial";
-  } else {
-    nav[0].classList.remove("main-header__wrapperUp");
-    scrollUp[0].style.display = "none";
-  }
-  previousScrollTop = scrollTop;
+    var scrollTop = window.scrollY;
+    if (scrollTop > previousScrollTop) {
+        nav[0].classList.add('main-header__wrapperUp');
+        scrollUp[0].style.display = 'initial';
+    } else {
+        nav[0].classList.remove('main-header__wrapperUp');
+        scrollUp[0].style.display = 'none';
+    }
+    previousScrollTop = scrollTop;
 }
 
-document.addEventListener(
-  "scroll",
-  function () {
+document.addEventListener('scroll', function() {
     isScrolling = true;
-  },
-  false
-);
+}, false);
 
-setInterval(function () {
-  if (isScrolling) {
-    hasScrolled();
-    isScrolling = false;
-  }
-}, 100);
+  setInterval(function() {
+    if (isScrolling) {
+      hasScrolled();
+      isScrolling = false;
+    }
+  }, 100);
