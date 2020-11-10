859548e25d6670a5a6d0f076715d9497643dce9e
Adrian Kotliński <kotlinski95@gmail.com>
2020-11-10 12:38:20 +0100

    Correct blog-animation script - reduce similar code and use object to change styles in blog content

--- js/blog-animation.js
+++ js/blog-animation.js
@@ -2,26 +2,30 @@
 
 function postToggle(postNumber) {
     var postButton = document.getElementById(`post-${postNumber}`);
-    var post = document.getElementsByClassName('main-blog__post');
-    var content = post[postNumber - 1].getElementsByClassName('read-more-target');
-    var paragraph = post[postNumber - 1].getElementsByClassName('main-blog-p');
+    var post = document.getElementsByClassName("main-blog__post");
+    var content = post[postNumber - 1].getElementsByClassName("read-more-target");
+    var paragraph = post[postNumber - 1].getElementsByClassName("main-blog-p");
+
+    const blog = {
+        Properties: ["0px", "0", "0", "none", "none", "0px", "0"],
+        setProperties() {
+            content[0].style.maxHeight = this.Properties[0];
+            content[0].style.opacity = this.Properties[1];
+            content[0].style.fontSize = this.Properties[2];
+            content[0].style.display = this.Properties[3];
+            paragraph[0].style.display = this.Properties[4];
+            paragraph[0].style.maxHeight = this.Properties[5];
+            paragraph[0].style.opacity = this.Properties[6];
+        }
+    }
+
+    var myBlog = Object.create(blog);
 
     if (postButton.checked) {
-        content[0].style.maxHeight = '2000px';
-        content[0].style.opacity = '1';
-        content[0].style.fontSize = 'inherit';
-        content[0].style.display = 'initial';
-        paragraph[0].style.display = 'flex';
-        paragraph[0].style.maxHeight = '100%';
-        paragraph[0].style.opacity = '1';
-    }
-    else {
-        content[0].style.maxHeight = '0px';
-        content[0].style.opacity = '0';
-        content[0].style.fontSize = '0';
-        content[0].style.display = 'none';
-        paragraph[0].style.display = 'none';
-        paragraph[0].style.maxHeight = '0px';
-        paragraph[0].style.opacity = '0';
+        myBlog.Properties = ["2000px", "1", "inherit", "initial", "flex", "100%", "1"];
+        myBlog.setProperties();
+    } else {
+        myBlog.Properties = ["0px", "0", "0", "none", "none", "0px", "0"];
+        myBlog.setProperties();
     }
 }
